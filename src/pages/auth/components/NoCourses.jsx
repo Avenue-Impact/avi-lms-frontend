dbfea2e14b21b36f917335499989c027aedde68f
Lugacity01 <yinkaabeebadesina@gmail.com>
2024-08-09 11:53:50 +0100

    latestupdate

--- src/pages/auth/components/NoCourses.jsx
+++ src/pages/auth/components/NoCourses.jsx
@@ -12,7 +12,7 @@ const NoCoursesMessage = () => {
       <img src={NoCoursesImg} alt="No Courses" className="w-32 h-32 mb-4 rounded-full" />
       <h3 className="text-2xl font-semibold text-gray-800 mb-2">No Courses</h3>
       <p className="text-center text-sm text-gray-600 mb-4">
-        It looks like you haven't enrolled in any courses yet. <br/> Explore our wide range of courses and start learning today!
+        It looks like you haven't enrolled in any courses yet. <span className='lg:block'> Explore our wide range of courses and start learning today!</span>
       </p>
       <Link to="/Dashboard/Dashboard_Discover">
         <DashButton className="mt-2 bg-[#CC1747] text-white hover:bg-[#b30e3b]">
