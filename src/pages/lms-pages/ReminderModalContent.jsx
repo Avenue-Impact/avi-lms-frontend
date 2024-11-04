a7eed4302559c1ef8078bddced5834721fec9976
lugacity1 <yinkaabeebadesina@gmail.com>
2024-11-04 08:40:43 -0800

    publish of course creation

--- src/pages/lms-pages/ReminderModalContent.jsx
+++ src/pages/lms-pages/ReminderModalContent.jsx
@@ -16,6 +16,7 @@ const ModalContent = ({ setShowModal }) => {
     setSelectedDay((prevSelectedDay) => (prevSelectedDay === day ? '' : day));
   };
 
+  
   const handleNextClick = () => {
     setShowAdditionalContent(true);
   };
