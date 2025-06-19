586121927cfac30d546ee6703b49f8219f3fbccc
Emray <mhatons@gmail.com>
2025-06-19 15:13:24 +0100

    updated code

--- src/pages/auth/components/ConfirmEmail.jsx
+++ src/pages/auth/components/ConfirmEmail.jsx
@@ -272,7 +272,7 @@ const ConfirmEmail = ({ setConfirm, setModal, setSuccess, user, form }) => {
           >
             Email
           </button>
-          <button
+          {/* <button
             className={`px-4 py-2 rounded ${
               deliveryMethod === 'sms' ? 'bg-primary-color-600 text-white' : 'bg-gray-200'
             }`}
@@ -280,7 +280,7 @@ const ConfirmEmail = ({ setConfirm, setModal, setSuccess, user, form }) => {
             disabled={isLoading}
           >
             SMS
-          </button>
+          </button> */}
         </div>
 
         <div className="mx-auto w-fit">
