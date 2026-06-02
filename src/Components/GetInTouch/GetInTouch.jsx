421c354467a3c51fd816e57bc1113e323bc0855a
Joshua Opeyemi <fakeyejoshua2005@gmail.com>
2026-06-02 11:55:26 +0100

    feat: adjust GetInTouch section layout with reduced padding, comment out descriptive text, and align elements vertically

--- src/Components/GetInTouch/GetInTouch.jsx
+++ src/Components/GetInTouch/GetInTouch.jsx
@@ -90,20 +90,20 @@ const GetInTouch = () => {
 
   return (
     <div
-      className={`${styles.get_in_touch} px-8 py-10 pt-20 lg:px-14 lg:pt-28 2xl:px-20 2xl:py-[100px]`}
+      className={`${styles.get_in_touch} px-8 py-10 pt-20 lg:px-14 lg:pt-28 2xl:px-20 2xl:py-[50px]`}
     >
       <div className={styles.get_in_touch_info}>
         <h4 className="text-2xl font-light 2xl:text-[40px] 2xl:leading-[40px]">
           Elevate Your Business with Avenue Impact
         </h4>
-        <p className="2xl:text-xl 2xl:font-light">
+        {/* <p className="2xl:text-xl 2xl:font-light">
           Let us be your partner in elevating your business to new levels of
           success. Our team of knowledgeable experts will collaborate with you
           to comprehend your specific needs and aspirations, and furnish
           customised solutions that promote growth and achievement. Get in touch
           with us today to embark on this exciting journey towards a thriving
           business.
-        </p>
+        </p> */}
       </div>
 
       <form className={styles.get_in_touch_input} onSubmit={handleSubmit} noValidate>
